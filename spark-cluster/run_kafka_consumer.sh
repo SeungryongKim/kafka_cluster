a3e7033b2db0d8758b4140e924d8b9e60a161f5b
SeungryongKim <srkim@euclidsoft.co.kr>
2020-10-20 10:28:50 +0900

    Edit quote

--- spark-cluster/run_kafka_consumer.sh
+++ spark-cluster/run_kafka_consumer.sh
@@ -4,9 +4,9 @@ master_cid=$(docker ps --filter name=master -q)
 kafka_ip=${1}
 
 # Just one master container excutes command.
-docker exec -it "$master_cid" sh -c "/spark/bin/spark-submit --master spark://spark-master:7077 --jars /kafka-consumer/spark-streaming-kafka-0-8-assembly_2.11-2.4.5.jar /kafka-consumer/direct_kafka_wordcount.py `$kafka_ip` 9092,9093,9094 test"
+docker exec -it "$master_cid" sh -c "/spark/bin/spark-submit --master spark://spark-master:7077 --jars /kafka-consumer/spark-streaming-kafka-0-8-assembly_2.11-2.4.5.jar /kafka-consumer/direct_kafka_wordcount.py $kafka_ip 9092,9093,9094 test"
 
 echo $kafka_ip
-echo "/spark/bin/spark-submit --master spark://spark-master:7077 --jars /jars/spark-streaming-kafka-0-8-assembly_2.11-2.4.5.jar /spark/direct_kafka_wordcount.py `$kafka_ip` 9092,9093,9094 test"
-~
+echo "/spark/bin/spark-submit --master spark://spark-master:7077 --jars /jars/spark-streaming-kafka-0-8-assembly_2.11-2.4.5.jar /spark/direct_kafka_wordcount.py $kafka_ip 9092,9093,9094 test"
+
 
